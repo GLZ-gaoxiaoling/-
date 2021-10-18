9e5925b7fe562916a142e0a34409b2a1dcca4138
喝杯热饮料 <82015116+Hebeireyinliao@users.noreply.github.com>
2021-10-18 18:08:08 +0800

    Add files via upload

--- 后端源码/猜字游戏.cpp
+++ 后端源码/猜字游戏.cpp
@@ -34,7 +34,6 @@ int main()
                 else if (guess > num)   cout << "答案太大了哟!!" << endl;
                 else    cout << "答案太小了哟!!" << endl;
                 cycele++;
-                frequency--;
             }while (guess < 0 || guess > 30);
             while (cycele <= chance && !found)
             {
@@ -47,8 +46,12 @@ int main()
                 cycele++;
                 frequency--;
             }
-            if (found)  cout << "居然让你猜对了  QWQ";
-            else    cout << "哎呀结束了" << endl << "没事的下次一定可以的  QWQ";
+            if (found)  cout << "居然让你猜对了  QWQ" << endl << endl;
+            else    cout << "哎呀结束了" << endl << "没事的下次一定可以的  QWQ" << endl << endl;
+            getchar();
+            cout << "按任意键退出" << endl;
+            getchar();
+            system("exit");
             break;
         }
         break;
@@ -79,27 +82,31 @@ int main()
                 cycele++;
                 frequency--;
             }
-            if (found)  cout << "你居然猜对了" << endl << "你的实力一定很厉害吧  QWQ";
-            else    cout << "哎呀呀   机会用完了哎" << endl << "没事再来一次一定可以的  QWQ";
+            if (found)  cout << "你居然猜对了" << endl << "你的实力一定很厉害吧  QWQ" << endl << endl;
+            else    cout << "哎呀呀   机会用完了哎" << endl << "没事再来一次一定可以的  QWQ" << endl << endl;
+            getchar();
+            cout << "按任意键退出" << endl;
+            getchar();
+            system("exit");
             break;
         }
         break;
 
         case 3:
-        least = 10,  most = 30, cycele = 1, chance = frequency = 3;
+        least = 0,  most = 20, cycele = 1, chance = frequency = 3;
         while (cycele <= chance && !found)
         {
             do
             {
                 num = temp % (most - least + 1) + least;
-                cout << "请输入一个10-30的随机数:";
+                cout << "请输入一个0-20的随机数:";
                 cin >> guess;
                 if (guess == num)   found = true;
                 else if (guess > num)   cout << "这个答案有点大了吧  QWQ" << endl;
                 else    cout << "这个答案有点小了吧  QWQ" << endl;
                 cycele++;
                 frequency--;
-            } while (guess < 10 || guess > 30);
+            } while (guess < 0 || guess > 20);
             while (cycele <= chance && !found)
             {
                 cout << "你还有" << frequency << "次机会哟" << endl << "一定要相信自己  QWQ" << endl;
@@ -111,8 +118,12 @@ int main()
                 cycele++;
                 frequency--;
             }     
-            if (found) cout << "哦吼!!!" << endl << "居然猜对了" << endl << "你一定玩的是简单模式  哼  QWQ";
-            else    cout << "好可惜差一点就猜对了" << endl << "没事好多事情都不是一帆风顺的" << endl << "相信自己下次一定可以的  QWQ" << endl << "我来告诉你正确答案吧" << endl << "是" << num << "没想到吧  嘿嘿" << endl;
+            if (found) cout << "哦吼!!!" << endl << "居然猜对了" << endl << "你一定玩的是简单模式  哼  QWQ" << endl << endl;
+            else    cout << "好可惜差一点就猜对了" << endl << "没事好多事情都不是一帆风顺的" << endl << "相信自己下次一定可以的  QWQ" << endl << "我来告诉你正确答案吧" << endl << "是" << num << "没想到吧  嘿嘿" << endl << endl;
+            getchar();
+            cout << "按任意键退出" << endl;
+            getchar();
+            system("exit");
             break;
         }
         break;
@@ -124,8 +135,12 @@ int main()
         cout << "欢迎来到赌神联盟" << endl << "成败在此一举" << endl;
         cout << "请输入一个10-25的随机数:";
         cin >> guess;
-        if (guess == num)   cout << "快说是不是开了" << endl << "难道这就是传说中的赌神  QWQ" << endl << "但是我还是感觉你开了   嘿嘿QWQ";
-        else    cout << "实力不行呀" << endl << "就是逊啊" << endl << "想知道答案吗  QWQ" << endl << "就不告诉你  QWQ";
+        if (guess == num)   cout << "快说是不是开了" << endl << "难道这就是传说中的赌神  QWQ" << endl << "但是我还是感觉你开了   嘿嘿QWQ" << endl << endl;
+        else    cout << "实力不行呀" << endl << "就是逊啊" << endl << "想知道答案吗  QWQ" << endl << "就不告诉你  QWQ" << endl << endl;
+        getchar();
+        cout << "按任意键退出" << endl;
+        getchar();
+        system("exit");
         break;
 
         case 5:
@@ -153,7 +168,6 @@ int main()
                     else if (guess > num)   cout << "这个数有点大了哦!!" << endl;
                     else    cout << "这个数有点小了哦!!" << endl;
                     cycele++;
-                    frequency--;
                 }while(guess < least || guess > most);
                 while (cycele <= chance && !found)
                 {
@@ -166,13 +180,21 @@ int main()
                     cycele++;
                     frequency--; 
                 }
-                if (found)  cout << "恭喜你猜对啦" << endl << "还是自定义符合你的心意呀";
-                else    cout << "没关系的设置的太难啦" << endl << "可以在来一遍呀";
+                if (found)  cout << "恭喜你猜对啦" << endl << "还是自定义符合你的心意呀" << endl << endl;
+                else    cout << "没关系的设置的太难啦" << endl << "可以在来一遍呀" << endl << endl;
+                getchar();
+                cout << "按任意键退出" << endl;
+                getchar();
+                system("exit");
                 break;
             }
             break;
         }
-        else    cout << "祝你游玩开心" << endl << "谢谢";
+        else    cout << "祝你游玩开心" << endl << "谢谢" << endl << endl;
+        getchar();
+        cout << "按任意键退出" << endl;
+        getchar();
+        system("exit");
         break;
     }
 }
